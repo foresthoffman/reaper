3554883d4508578902df531d5f631dbb8a937a6e
Forest Hoffman <foresthoffman@users.noreply.github.com>
2017-10-01 19:16:52 -0700

    Add LICENSE

--- main.go
+++ main.go
@@ -2,6 +2,8 @@
  * main.go
  *
  * Copyright (c) 2017 Forest Hoffman. All Rights Reserved.
+ * License: MIT License (see the included LICENSE file) or download at
+ *     https://raw.githubusercontent.com/foresthoffman/midprocrunner/master/LICENSE
  */
 
 package main
